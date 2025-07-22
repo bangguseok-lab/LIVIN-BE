91c8ce4dd2d970223c80f2974c976d88b5ca44d5
Jeon-Yeongtae <taehyeong17863@gmail.com>
2025-07-22 22:26:06 +0900

    :sparkles: Feat: 예외 처리 클래스 기능 구현

--- src/main/java/org/livin/exception/MainPageException.java
+++ src/main/java/org/livin/exception/MainPageException.java
@@ -1,5 +1,36 @@
 package org.livin.exception;
 
-public class MainPageException {
+public class MainPageException extends RuntimeException {
+    public MainPageException() {
+        super("인증되지 않은 사용자입니다.");
+    }
 
+    public MainPageException(String message) {
+        super(message);
+    }
+}
+
+// 리소스를 찾을 수 없는 예외
+class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
+
+// 잘못된 요청 예외
+class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
+
+// 서버 내부 오류 예외
+class InternalServerException extends RuntimeException {
+    public InternalServerException(String message) {
+        super(message);
+    }
+
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
