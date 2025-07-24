34225aaddf44048971d2fa7dd49f5151dcfb6c9c
Jeon-Yeongtae <taehyeong17863@gmail.com>
2025-07-25 00:28:24 +0900

    :sparkles: Feat: 메인페이지 관심 매물을 출력할 때, 회원이 관심으로 등록한 매물들을 List 형태로 먼저 출력 받도록 구현

---
+++ src/main/java/org/livin/mapper/FavoritePropertyMapper.java
@@ -0,0 +1,22 @@
+package org.livin.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.livin.vo.Property;
+
+import java.util.List;
+
+@Mapper
+public class FavoritePropertyMapper {
+
+    // 1) user_id로 관심으로 등록된 매물들 id를 조회
+    List<Long> getFavoritePropertyIdsByUserId(Long userId);
+
+    // 2) user_id를 통해 관심 매물들 id들을 getFavoritePropertyByUserId로 받아왔다.
+    // 3) 이제 해당 매물 id List를 바탕으로 하나씩 Property 정보들을 받아오면 된다. (+ 최신순, 3개)
+    List<Property> getPropertiesByIdsSorted(
+            @Param("propertyIds") List<Long> propertyIds,
+            @Param("limit") int limit
+    );
+
+}
