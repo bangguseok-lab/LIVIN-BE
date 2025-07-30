56f7f9e03161813c7e4a81d80dfad52894ede1aa
Youngbae1126 <devhyun1126@gmail.com>
2025-07-31 03:28:18 +0900

    :sparkles: Feat: 체크리스트 생성 후 상세 조회에 필요한 DTO 구현 - ChecklistDetailDTO: 체크리스트 + 체크리스트 아이템 조인 결과를 아이템의 타입별로 묶어서 저장하기 위한 DTO - ChecklistItemJoinDTO: 체크리스트 + 체크리스트 아이템 조인 결과를 한 줄 한 줄 저장하기 위한 DTO - ChecklistItemSimpleDTO: 체크리스트 아이템 중에서 필요한 정보만 가져오는 DTO

---
+++ src/main/java/org/livin/checklist/dto/ChecklistItemSimpleDTO.java
@@ -0,0 +1,13 @@
+package org.livin.checklist.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+// 각 아이템에서 필요한 최소 필드만 포함
+@Builder
+@Getter
+public class ChecklistItemSimpleDTO {
+	private Long checklistItemId;
+	private String keyword;
+	private boolean isActive;
+}
