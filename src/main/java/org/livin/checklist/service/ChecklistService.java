5a0e539b62a78261f3e90147dbe6928b3bcc2b4b
Youngbae1126 <devhyun1126@gmail.com>
2025-07-31 03:23:57 +0900

    :sparkles: Feat: 체크리스트 생성 및 전체 조회 서비스 로직 구현

--- src/main/java/org/livin/checklist/service/ChecklistService.java
+++ src/main/java/org/livin/checklist/service/ChecklistService.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import org.livin.checklist.dto.ChecklistCreateRequestDTO;
 import org.livin.checklist.dto.ChecklistDTO;
+import org.livin.checklist.dto.ChecklistDetailDTO;
 
 public interface ChecklistService {
 	// 체크리스트 전체 목록 조회
 	List<ChecklistDTO> getAllList(Long userId);
 
 	// 체크리스트 상세 조회
-	// ChecklistDTO getDetailList(Long checklistId);
+	ChecklistDetailDTO getChecklistDetail(Long checklistId);
 
 	// 체크리스트 생성
-	ChecklistDTO createChecklist(ChecklistCreateRequestDTO checklistDto, Long userId);
+	ChecklistDetailDTO createChecklist(ChecklistCreateRequestDTO checklistDto, Long userId);
+
 }
--- src/main/java/org/livin/checklist/service/ChecklistServiceImpl.java
+++ src/main/java/org/livin/checklist/service/ChecklistServiceImpl.java
@@ -6,9 +6,12 @@ import java.util.stream.Collectors;
 
 import org.livin.checklist.dto.ChecklistCreateRequestDTO;
 import org.livin.checklist.dto.ChecklistDTO;
+import org.livin.checklist.dto.ChecklistDetailDTO;
+import org.livin.checklist.dto.ChecklistItemJoinDTO;
 import org.livin.checklist.entity.ChecklistVO;
 import org.livin.checklist.mapper.ChecklistMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -28,8 +31,18 @@ public class ChecklistServiceImpl implements ChecklistService {
 			.collect(Collectors.toList());
 	}
 
+	// 체크리스트 상세 조회
+
 	@Override
-	public ChecklistDTO createChecklist(ChecklistCreateRequestDTO checklistDto, Long userId) {
+	public ChecklistDetailDTO getChecklistDetail(Long checklistId) {
+		List<ChecklistItemJoinDTO> joinRows = checklistMapper.getChecklistDetail(checklistId);
+		return ChecklistDetailDTO.from(joinRows);
+	}
+
+	// 체크리스트 생성
+	@Transactional    // 전체 과정이 하나의 트랜잭션으로 처리되도록 보장하는 어노테이션
+	@Override
+	public ChecklistDetailDTO createChecklist(ChecklistCreateRequestDTO checklistDto, Long userId) {
 		// DTO -> VO 변환
 		ChecklistVO checklistVO = ChecklistVO.builder()
 			.userId(userId)
@@ -42,15 +55,26 @@ public class ChecklistServiceImpl implements ChecklistService {
 
 		log.info("============> request VO: {}", checklistVO);
 
-		// DB insert 작업
 		try {
+			// Checklist 생성
 			checklistMapper.create(checklistVO);
-			log.info("✅ 생성된 checklistId: {}", checklistVO.getChecklistId());
+			Long checklistId = checklistVO.getChecklistId();    // 생성된 checklistId 추출
+			log.info("✅ 생성된 checklistId: {}", checklistId);
+
+			// ChecklistItem 기본 아이템 생성
+			checklistMapper.createChecklistDefaultItem(checklistId);
+
+			// Checklist + ChecklistItem 테이블 조인해서 상세정보 함께 반환
+			List<ChecklistItemJoinDTO> joinList = checklistMapper.getChecklistDetail(checklistId);
+			ChecklistDetailDTO resultDto = ChecklistDetailDTO.from(joinList);
+
+			log.info("✅ 생성된 resultDTO: {}", resultDto);
+
+			return resultDto;
+
 		} catch (Exception e) {
 			log.error("============> 체크리스트 생성 중 에러 발생", e);
 			throw new RuntimeException("체크리스트 생성 실패", e); // 예외를 던져야 controller에서 500 처리 가능
 		}
-		// VO → DTO 변환 후 반환
-		return ChecklistDTO.of(checklistVO);
 	}
 }
