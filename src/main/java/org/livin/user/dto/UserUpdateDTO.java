be60b0ae5bd48a62467cbd6acf6feb52a6a1e5dd
Youngbae1126 <devhyun1126@gmail.com>
2025-08-05 01:30:46 +0900

    :recycle: Refactor: 유저 관련 코드 리팩토링 - User 관련 DTO 추가 - User Service 인터페이스와 구현 클래스 분리 - User 관련 기능 추가

---
+++ src/main/java/org/livin/user/dto/UserUpdateDTO.java
@@ -0,0 +1,14 @@
+package org.livin.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class UserUpdateDTO {
+	private Long userId;
+	private String nickname;
+	private String phone;
+	private int profileImage;
+}
