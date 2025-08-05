8aeb81a3d2651557745da3b1016c3c66ea80e8b0
imnjb@naver.com <imnjb@naver.com>
2025-08-05 15:06:16 +0900

    UserUpdateDTO 수정

--- src/main/java/org/livin/user/dto/UserUpdateDTO.java
+++ src/main/java/org/livin/user/dto/UserUpdateDTO.java
@@ -1,10 +1,14 @@
 package org.livin.user.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class UserUpdateDTO {
 	private Long userId;
