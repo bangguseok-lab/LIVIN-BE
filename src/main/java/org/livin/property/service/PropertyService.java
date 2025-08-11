88ec16704bd90bdc5b9798d0b604f381ed94a8d1
조아빈 <whdkqls122@naver.com>
2025-08-11 09:45:13 +0900

    :recycle: Refactor: rebase 충돌 해결

--- src/main/java/org/livin/property/service/PropertyService.java
+++ src/main/java/org/livin/property/service/PropertyService.java
@@ -13,7 +13,7 @@ public interface PropertyService {
 
 	public List<PropertyDTO> getPropertiesByRegion(FilteringDTO address);
 
-	public long  countProperties(FilteringDTO address);
+	public long countProperties(FilteringDTO address);
 
 	PropertyDetailsDTO getPropertyDetails(Long propertyId, String providerId);
 
@@ -23,8 +23,7 @@ public interface PropertyService {
 	// ✅ 관심 매물 삭제를 위한 메서드 추가
 	public void removeFavoriteProperty(Long propertyId, Long userId);
 
-	// ✅ 관심 매물 추가를 위한 메서드 추가
-	public void addFavoriteProperty(Long userId, Long propertyId);
-
 	OwnerInfoResponseDTO getRealEstateRegisters(OwnerInfoRequestDTO ownerInfoRequestDTO);
+
+	public PropertyDTO addFavoriteProperty(Long userId, Long propertyId);
 }
