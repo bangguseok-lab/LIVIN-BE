f7bef2271a22c298ea7297a33302e5b6366f10cd
조아빈 <whdkqls122@naver.com>
2025-08-13 02:45:40 +0900

    sparkles: Feat: 매물등록 요청 DTO 추가

--- src/main/java/org/livin/property/dto/PropertyImgRequestDTO.java
+++ src/main/java/org/livin/property/dto/PropertyImgRequestDTO.java
@@ -1,2 +1,17 @@
-package org.livin.property.dto;public class PropertyImgRequestDTO {
+package org.livin.property.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Builder
+public class PropertyImgRequestDTO {
+	private MultipartFile img;
+	private Boolean represent;
 }
