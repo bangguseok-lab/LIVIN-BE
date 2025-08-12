fcac839e613a58f139b8ffa8f5e1ce8f96a7d0d4
JeonYeongtae <liminal.yeongtae@gmail.com>
2025-08-12 14:55:13 +0900

    :recycle: Refactor: 매물 상세 페이지 체크리스트 아이템(옵션) 조회에 사용하는 DTO 필드를 DB명과 일치하도록 변경

--- src/main/java/org/livin/property/dto/ChecklistItemDTO.java
+++ src/main/java/org/livin/property/dto/ChecklistItemDTO.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class ChecklistItemDTO {
 	private Long checklistItemId; // PK
 	private String keyword;       // 옵션 키워드
-	private Boolean active;       // is_active
-	private Boolean checked;      // is_checked
+	private Boolean isActive;       // is_active
+	private Boolean isChecked;      // is_checked
 	private String type;          // ROOM/BUILDING/OPTION/INFRA/CIRCUMSTANCE/CUSTOM
 	private Long checklistId;     // FK
 }
